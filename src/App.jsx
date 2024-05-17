57eb4609a2c80dd8aabf36f722067096e173ffe5
Sami <saminazirqureshi1@gmail.com>
2024-05-17 23:23:54 +0500

    working

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Button } from "antd";
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+    <p>sa</p>
+    <Button type="primary">Primary Button</Button>
+    </>
+  )
+}
+
+export default App
